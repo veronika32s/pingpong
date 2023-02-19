6b39583695f89cece0d5ebad7cd0be9e354776f7
veronika32s <veronikakovalova5@gmail.com>
2023-02-19 15:28:58 +0200

    first

--- game_object.py
+++ game_object.py
@@ -5,4 +5,11 @@ class Board(pygame.Rect):
         super().__init__(x ,y , width, heigth)
         self.IMAGE = image
         self.SPEED = speed
-        self.MOVE = {"UP"}
+        self.MOVE = {"UP":False,"DOWN":False}
+
+    def move(self):
+        if self.MOVE["UP"] and self.y > 0 :
+            self.y -= self.SPEED
+        elif self.MOVE["DOWN"] and self.y < 400:
+            self.y += self.SPEED
+        #print(1)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame 
+from game_object import*
 
 pygame.init()
 
@@ -7,11 +8,27 @@ pygame.display.set_caption("ping-pong")
 
 def run():
     game = True 
+    clock = pygame.time.Clock()
+    player1 = Board(15,250 ,20, 100,None, 5)
 
     while game:
+        window.fill((0,0,0))
+        pygame.draw.rect(window, (255,0,0),player1)
+        player1.move()
 
-        for event in pygame.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game = False
-
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player1.MOVE["UP"] = True
+                if event.key == pygame.K_s:
+                    player1.MOVE["DOWN"] = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    player1.MOVE["UP"] = False
+                if event.key == pygame.K_s:
+                    player1.MOVE["DOWN"] = False        
+        clock.tick(60)
+        pygame.display.flip()
 run()
